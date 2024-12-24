79bd9fcafead09a4976b31ffc3b26f524be8968c
gowtham258 <gowthamaryan405@gmail.com>
2024-12-24 18:04:57 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+def a(b,c,d):
+  if d=='+':
+    return b+c
+  elif d=='-':
+    return abs(b-c)
+  elif d=='*':
+    return b*c
+  else:
+    return b/c
+print(a(45,97,'+'))
+print(a(45,97,'-'))
+print(a(45,97,'*'))
+print(a(45,97,'quotient'))
+import math
+def u(v,w):
+  return math.sqrt(v**2+w**2)
+print(u(52,142))
+print(u(0,4365))
+def t(s,d):
+  return s/d
+print(t(151.21,1.87))
+# for 151 kilometers we are going in 80 minutes we can reach the cave before sunset
+# y=76*22*18
+# def a(y):
+#   for i in range (2,y):
+#     for j in range (i+1,)
+  
+  
+  
+  
+  
+print(45,97,1.87142,52,4365,0.4639175257731959,151.22169156572744,4365.0,80.86096256684492)
